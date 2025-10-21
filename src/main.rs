8855110ff2a8909a4907941fd95a9c2a2fc1e485
Nathan Gill <nathan.j.gill@outlook.com>
2025-10-21 21:52:01 +0100

    add basic parser for frontmatter

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
+use copy_dir::copy_dir;
 use pulldown_cmark::{Options, Parser};
+use serde::Deserialize;
 use std::{
     fs::{create_dir_all, read_to_string, remove_dir_all, write},
     path::Path,
 };
 use walkdir::WalkDir;
-use copy_dir::copy_dir;
+
+#[derive(Debug, Deserialize, Default)]
+pub struct FrontMatter {
+    title: Option<String>,
+}
+
+fn split_frontmatter(content: &str) -> (FrontMatter, &str) {
+    let content = content.trim_start();
+    if content.starts_with("---") {
+        if let Some(end) = content[3..].find("---") {
+            let fm_str = &content[3..3 + end];
+            let body = &content[3 + end + 3..];
+            let fm = serde_yaml::from_str(fm_str).unwrap_or_default();
+            return (fm, body);
+        }
+    }
+    (FrontMatter::default(), content)
+}
 
 fn main() {
     let src_root = Path::new("content");
@@ -32,10 +51,13 @@ fn main() {
 
         if entry_type.is_file() {
             let content = read_to_string(path).unwrap();
+            let (fm, body) = split_frontmatter(&content);
+
+            println!("{:#?}", fm);
 
             let mut options = Options::empty();
             options.insert(Options::ENABLE_YAML_STYLE_METADATA_BLOCKS);
-            let parser = Parser::new_ext(&content, options);
+            let parser = Parser::new_ext(body, options);
 
             let mut html_output = String::new();
             pulldown_cmark::html::push_html(&mut html_output, parser);
